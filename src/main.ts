871385e098dbe5ae58d18767fb39b285240567eb
HECTOR STEVEEN ORDOÑEZ CHAMBA <zh311505@gmail.com>
2023-12-15 17:19:45 -0500

    feat: cors para mandar solicitudes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: 'http://localhost:4200',  // Especifica el origen permitido
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.setGlobalPrefix("api/v1");
 
   app.useGlobalPipes(
